d32c8368dc2d48fcdc9eecc09bcdf2a70fff38e7
dtroberts <dtroberts@wpi.edu>
2014-10-14 15:02:20 -0500

    Added UnprotectedArea, ProtectedArea and Types enum.

---
+++ CyberPoetrySlam/src/entity/ProtectedArea.java
@@ -0,0 +1,68 @@
+package model;
+import java.util.ArrayList;
+
+public class ProtectedArea {
+	ArrayList<Word> words;
+	ArrayList<Poem> poems;
+	public static ProtectedArea instance;
+	
+	private ProtectedArea() {
+		
+	}
+	
+	public ProtectedArea getInstance() {
+		if (instance == null) {
+			instance = new ProtectedArea();
+		}
+		return instance;
+	}
+	
+	public boolean add(Word word) {
+		return words.add(word);
+	}
+	
+	public boolean remove(Word word) {
+		int index = words.indexOf(word);
+		if (index == -1) {							// word not found
+			return false;
+		}
+		else {
+			words.remove(index);
+			return true;
+		}
+	}
+	
+	// false if unsuccessful; true otherwise 
+	public boolean moveEntity(Entity e, int x, int y) {
+		if (doesIntersect(e)) {				// invalid
+			return false;
+		}
+		e.x = x;
+		e.y = y;
+		return true;
+	}
+	
+	private boolean doesIntersect(Entity e) {
+		// compare it based on location and area of e
+		for (Word word : words) {		// lazy; maybe do cleaner
+			if (word.intersect(e.x, e.y) == true) {
+				return true;
+			}
+		}
+		
+		for (Poem poem : poems) {		// lazy; maybe do cleaner
+			if (poem.intersect(e.x, e.y) == true) {
+				return true;
+			}
+		}
+		/* For use in Word and Poem; preliminary version which does not return direction
+		 intersect(Entity e) {
+		 	if ((((e.x >= this.x) && (e.x < (this.x + this.width))) || ((this.x >= e.x) && (this.x < (e.x + e.width)))) &&
+				  	(((e.y >= this.y) && (e.y < (this.y + this.height))) || ((this.y >= e.y) && (this.y < (e.y + e.height))))) {
+				return true;
+			}
+		 */
+		return false;
+	}
+	
+}
---
+++ CyberPoetrySlam/src/entity/UnprotectedArea.java
@@ -0,0 +1,55 @@
+package model;
+import java.util.ArrayList;
+
+
+public class UnprotectedArea {
+	public static UnprotectedArea instance;
+	ArrayList<Word> words;
+	
+	private UnprotectedArea() {
+		
+	}
+	
+	public UnprotectedArea getInstance() {
+		if (instance == null) {
+			instance = new UnprotectedArea();
+		}
+		return instance;
+	}
+	
+	public boolean addToUnprotected(Word word) {
+		words.add(word);
+		return true;
+	}
+	
+	public boolean removeFromUnprotected(Word word) {
+		int index = words.indexOf(word);
+		if (index == -1) {							// word not found
+			// do something
+		}
+		else {
+			words.remove(index);
+		}
+		return true;
+	}
+	
+	public ArrayList<Word> searchUnprotectedArea(String str) {
+		ArrayList<Word> validWords = new ArrayList<Word>();
+		for (Word word : words) {
+			if (word.value.contains(str)) {
+				validWords.add(word);
+			}
+		}
+		return validWords;
+	}
+	
+	public ArrayList<Word> filterUnprotectedArea(Types type) {
+		ArrayList<Word> validWords = new ArrayList<Word>();
+		for (Word word : words) {
+			if (word.type == type) {
+				validWords.add(word);
+			}
+		}
+		return validWords;
+	}
+}
