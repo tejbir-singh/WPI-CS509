3959c17be4c36e89f5d2c72587b6ef9ecb54af2d
dtroberts <dtroberts@wpi.edu>
2014-11-05 18:55:49 -0500

    Fixed a small bug in one of the tests (no change to any other code).

--- CyberPoetrySlam/test/cps/controller/ConnectWordControllerTest.java
+++ CyberPoetrySlam/test/cps/controller/ConnectWordControllerTest.java
@@ -1,11 +1,14 @@
 package cps.controller;
 
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 
 import cps.controller.ConnectWordController;
 import cps.model.GameManager;
 import cps.model.Type;
 import cps.model.Word;
+import cps.model.Poem;
 import cps.view.ApplicationPanel;
 import junit.framework.TestCase;
 
@@ -17,12 +20,22 @@ public class ConnectWordControllerTest extends TestCase {
 	@Override
 	protected void setUp() {
 		gm = GameManager.getInstance();
+		gm.getPa().setWords(new ArrayList<Word>());
+		gm.getPa().setPoems(new ArrayList<Poem>());
+		gm.getUa().setWords(new ArrayList<Word>());
 		app = new ApplicationPanel(gm, new JButton());
 		mc = new ConnectWordController(gm, app);
 		Word w1 = new Word(1, 1, 1, 1, Type.ADJECTIVE, "test1");
 		Word w2 = new Word(100, 100, 1, 1, Type.NOUN, "test2");
 		gm.getPa().add(w1);
 		gm.getPa().add(w2);
+	}
+	
+	@Override
+	protected void tearDown() {
+		gm.getPa().setWords(new ArrayList<Word>());
+		gm.getPa().setPoems(new ArrayList<Poem>());
+		gm.getUa().setWords(new ArrayList<Word>());
 	}
 	
 	public void testSelect() {
