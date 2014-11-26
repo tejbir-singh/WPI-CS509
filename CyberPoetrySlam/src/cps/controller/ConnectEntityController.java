0d7ff2f905b95fdbaafd90670ebdb9c3db35cb11
Tejbir Singh <tejbir_chawla@yahoo.com>
2014-11-26 02:41:21 -0500

    Check in changes to support the connectPoem Use Case. Refactored the ConnectWordController.java to ConnectEntityController.java.

--- CyberPoetrySlam/src/cps/controller/ConnectEntityController.java
+++ CyberPoetrySlam/src/cps/controller/ConnectEntityController.java
@@ -9,7 +9,7 @@ import cps.view.*;
 /**
  * @author Devin
  */
-public class ConnectWordController extends MouseAdapter {
+public class ConnectEntityController extends MouseAdapter {
 
 	/** Needed for controller behavior. */
 	GameManager gm;
@@ -30,7 +30,7 @@ public class ConnectWordController extends MouseAdapter {
 	int buttonType;
 	
 	/** Constructor holds onto key manager objects. */
-	public ConnectWordController(GameManager gm, ApplicationPanel panel) {
+	public ConnectEntityController(GameManager gm, ApplicationPanel panel) {
 		this.gm = gm;
 		this.panel = panel;
 	}
@@ -73,21 +73,33 @@ public class ConnectWordController extends MouseAdapter {
 
 		// pieces are returned in order of Z coordinate
 		Word w = gm.findWord(anchor.x, anchor.y);
-		
+
 		// TEMPORARY: Need to change to allow Poems to be connected to each other
-		if (w == null || gm.getPa().belongsToPoem(w) != null || w.getY() > GameManager.AREA_DIVIDER) { return false; }
-		
-		// no longer in the board since we are moving it around...
-		//gm.getUa().remove(w);
-		gm.getPa().remove(w);
-		gm.setSelected(w);
-		originalx = w.getX();
-		originaly = w.getY();
-			
+		if (w == null || w.getY() > GameManager.AREA_DIVIDER ) { return false; }
+
+		Poem tmp;
+		tmp = gm.getPa().belongsToPoem(w);
+
+		if ( tmp != null) /* selected poem */
+		{
+			gm.setSelected(tmp);
+			gm.getPa().remove(tmp);
+			originalx = tmp.getX();
+			originaly = tmp.getY();
+		}
+		else { /* selected a word */
+
+			// no longer in the board since we are moving it around...
+			//gm.getUa().remove(w);
+			gm.getPa().remove(w);
+			gm.setSelected(w);
+			originalx = w.getX();
+			originaly = w.getY();
+
+		}
 		// set anchor for smooth moving
 		deltaX = anchor.x - originalx;
 		deltaY = anchor.y - originaly;
-		
 		// paint will happen once moves. This redraws state to prepare for paint
 		panel.redraw();
 		return true;
@@ -96,39 +108,93 @@ public class ConnectWordController extends MouseAdapter {
 	/** Separate out this function for testing purposes. */
 	protected boolean drag (int x, int y) {
 		if (buttonType == MouseEvent.BUTTON3) { return false; }
-		Word selected = (Word) gm.getSelected();
+		Entity selected = gm.getSelected();
 		
 		if (selected == null) { return false; }
 		
+		if (selected instanceof Word) {
+			selected = (Word)gm.getSelected();
+			panel.paintWord((Word)selected);
+		}
+		else {
+			panel.paintPoem((Poem)selected);
+		}
+		/* handle poem instance */
 		panel.paintBackground(selected);
 		selected.setPosition(x - deltaX, y - deltaY);
-		panel.paintWord(selected);
+		
 		panel.repaint();
 
-		return true;
+		return true; 
 	}
 	
 	/** Separate out this function for testing purposes. */
 	protected boolean release (int x, int y) {
-		Word selected = (Word) gm.getSelected();
+		Entity selected = gm.getSelected();
+
 		if (selected == null) { return false; }
-		
+
+		if (selected instanceof Word) {
+			selected = (Word)gm.getSelected();
+			System.out.println("Selected Entity:" + selected.toString());
+		}
+		else {
+			selected = (Poem)gm.getSelected();
+			System.out.println("Selected Entity:" + selected.toString());
+		}
+
 		if (gm.getPa().entityIntersect(selected) == null) { 			// didn't select any word to connect
 			revert();
 		}
 		else {
-			ri = gm.getPa().entityIntersect(selected);
+			ri = gm.getPa().entityIntersect(selected);     // selected entity is either a word or a poem
+			
 			if(ri.idxPoem == -1){ 										//ri.w is a single word
-				connectWordToWord(selected);
+				connectWordToWord((Word)selected);
 			}
-			else{ 														// ri.w is a word in a poem
-				connectWordToPoem(selected);
-			}
-		}
-		
+			else{ 	//either connecting a word or poem to an existing poem				// ri.w is a word in a poem
+				Poem basePoem = ri.p; // get the base poem
+				//System.out.println("ri.idxPoem:" + ri.idxPoem);																																																																																																																																																																																																																
+				if (ri.p == null) {
+					//System.out.println("NULL Base Poem");
+				}
+				int basePoemX = basePoem.getX();
+				int basePoemY = basePoem.getY();
+				int basePoemHeight = basePoem.getHeight();
+				int basePoemBottomY = basePoemY + basePoemHeight/2;
+				int basePoemMidY = basePoemBottomY/2;
+				// System.out.println("basePoemX: " + basePoemX);
+				// System.out.println("basePoemY: " + basePoemY);
+				// System.out.println("Base Poem Height: " + basePoemHeight);
+				// System.out.println("Base Poem Bottom Y:" + basePoemBottomY);
+				// System.out.println("Half-way point for Poem:" + basePoemMidY);
+				
+				// decipher here if connecting a word | poem to an existing poem
+				if (selected instanceof Poem) {
+					// connect poem with poem
+					if (y < basePoemBottomY) {
+						Poem ptop = (Poem)selected;
+						int ptop_rows = ptop.rows.size(); // number of rows
+						Row bottomRowPTop = ptop.rows.get(ptop_rows -1);
+						int bottomRowX = bottomRowPTop.getX();
+						//System.out.println("bottomRowX:" + bottomRowX);
+						gm.getPa().connectPoemTop(basePoem, (Poem)selected, bottomRowX);
+					}
+					else {
+						Poem pBottom = (Poem)selected;
+						int pBottomX = pBottom.getX();
+						gm.getPa().connectPoemBottom(basePoem, pBottom, pBottomX);
+					}
+				}
+				else {
+					connectWordToPoem((Word)selected);
+				}
+			}																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																														
+		}																																																												
+
 		// no longer selected
 		gm.setSelected(null);
-		panel.redraw();
+		panel.redraw();																																																							
 		panel.repaint();
 		return true;
 	}
